ce776065935184fad7fe2074ef6ed928c61137b8
Bechu <vincentbechu@gmail.com>
2016-09-17 21:27:18 +0200

    some change

---
+++ src/UserBundle/Controller/UserController.php
@@ -0,0 +1,13 @@
+<?php
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+  public function settingAction(Request $request)
+  {
+    
+  }
+}
